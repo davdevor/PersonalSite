fd2ab1b9e84cce8a5cb5ff5778d63044333cedde
David Devore <drdcustomlows@gmail.com>
2018-05-22 15:57:07 -0400

    add views and methods to controller

---
+++ src/PersonalSite/PersonalSite/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("About");
+        }
+
+        public ViewResult ContactInfo()
+        {
+            return View();
+        }
+        public ViewResult About()
+        {
+            return View();
+        }
+
+        public ViewResult Projects()
+        {
+            return View();
+        }
+        public ViewResult Experience()
+        {
+            return View();
+        }
+
+        public PartialViewResult Resume()
+        {
+            return PartialView();
+        }
+    }
+}
