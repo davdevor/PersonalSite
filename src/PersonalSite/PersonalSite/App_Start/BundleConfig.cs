3fcd80c24b94e45f2ea1b856bed408b48115e26d
David Devore <drdcustomlows@gmail.com>
2018-05-25 08:43:16 -0400

    add bundling and minimization to scripts and css

---
+++ src/PersonalSite/PersonalSite/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+namespace PersonalSite.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            ScriptBundle thirdPartyScripts = new ScriptBundle("~/Scripts/ThirdParty");
+            thirdPartyScripts.Include("~/Scripts/jquery-1.10.2.min.js", "~/Scripts/bootstrap.min.js");
+            bundles.Add(thirdPartyScripts);
+            StyleBundle styles = new StyleBundle("~/Content/Bundle");
+            styles.Include("~/Content/bootstrap.min.css", "~/Content/bootstrap-theme.css", "~/Content/Site.css");
+            bundles.Add(styles);
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
