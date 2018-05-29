3034fbc46fb642d33faf6afd5d8ee8e770bee153
David Devore <drdcustomlows@gmail.com>
2018-05-29 08:00:16 -0400

    add script

---
+++ src/PersonalSite/PersonalSite/Scripts/sitescripts.js
@@ -0,0 +1,13 @@
+﻿var image1 = document.getElementById("githubIcon");
+var image2 = document.getElementById("linkedinIcon")
+makeOpaque(image1);
+makeOpaque(image2);
+
+function makeOpaque(image) {
+    image.addEventListener("mouseover", function () {
+        image.className = "img-opaque";
+    });
+    image.addEventListener("mouseout", function () {
+        image.className = "";
+    });
+}
