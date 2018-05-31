e2ef0f2fee4d67e38b9374a8e90c488ae92d5ecf
David Devore <drdcustomlows@gmail.com>
2018-05-31 14:35:12 -0400

    add scripts for resume and project to bundle

---
+++ src/PersonalSite/PersonalSite/Scripts/projectscripts.js
@@ -0,0 +1,21 @@
+﻿var idArray = ["personalsitelabel", "poetrylabel", "geneticlabel", "recipielabel", "equationlabel"];
+var labelArray = setupLabels(idArray);
+function setupLabels(idsArray) {
+    var labelArray = new Array();
+    var tempLabel;
+    for (i = 0; i < idsArray.length; ++i) {
+        tempLabel = document.getElementById(idsArray[i]);
+        changeLabelClassOnMouse(tempLabel);
+        tempLabel.addEventListener("click", function () {
+            window.open(this.dataset.link);
+        })
+    }
+}
+function changeLabelClassOnMouse(label) {
+    label.addEventListener("mouseover", function () {
+        label.className = "rounded mylabel-hover";
+    });
+    label.addEventListener("mouseout", function () {
+        label.className = "rounded mylabel";
+    });
+}
