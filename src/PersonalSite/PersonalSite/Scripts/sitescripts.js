4c88b6031972ea13d34a86fc4fdff1e7fe6d469c
David Devore <drdcustomlows@gmail.com>
2018-05-31 08:00:31 -0400

    add google analytics to js bundle

--- src/PersonalSite/PersonalSite/Scripts/sitescripts.js
+++ src/PersonalSite/PersonalSite/Scripts/sitescripts.js
@@ -1,7 +1,13 @@
-﻿var image1 = document.getElementById("githubIcon");
-var image2 = document.getElementById("linkedinIcon")
-makeOpaque(image1);
-makeOpaque(image2);
+﻿window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'UA-119836223-1');
+
+var githubImage = document.getElementById("githubIcon");
+var linkedinImage = document.getElementById("linkedinIcon")
+makeOpaque(linkedinImage);
+makeOpaque(githubImage);
 
 function makeOpaque(image) {
     image.addEventListener("mouseover", function () {
