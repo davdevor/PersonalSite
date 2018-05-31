84b25a5b3ebe3027c77e33e53e57fc0d2bd504d4
David Devore <drdcustomlows@gmail.com>
2018-05-31 08:19:31 -0400

    removed whitespace

--- src/PersonalSite/PersonalSite/App_Start/BundleConfig.cs
+++ src/PersonalSite/PersonalSite/App_Start/BundleConfig.cs
@@ -10,15 +10,6 @@ namespace PersonalSite.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
 
-            /*
-            https://stackoverflow.com/questions/23273996/using-cdn-in-asp-net-mvc-bundles
-            ScriptBundle thirdPartyScripts = new ScriptBundle("~/Scripts/ThirdParty");
-            thirdPartyScripts.Include("~/Scripts/jquery-1.10.2.min.js", "~/Scripts/bootstrap.min.js","~/Scripts/sitescripts");
-            bundles.Add(thirdPartyScripts);
-            StyleBundle styles = new StyleBundle("~/Content/Bundle");
-            styles.Include("~/Content/bootstrap.min.css", "~/Content/bootstrap-theme.css", "~/Content/Site.css");
-            bundles.Add(styles);*/
-
             bundles.Add(new ScriptBundle("~/Scripts/jquery", "https://code.jquery.com/jquery-1.12.4.min.js").Include("~/Scripts/jquery-1.10.2.min.js"));
             bundles.Add(new ScriptBundle("~/Scripts/bootstrapjs", "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js").Include("~/Scripts/bootstrap.min.js"));
             bundles.Add(new ScriptBundle("~/Scripts/sitescripts").Include("~/Scripts/sitescripts.js"));
