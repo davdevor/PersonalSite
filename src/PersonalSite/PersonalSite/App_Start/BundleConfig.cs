8a5161b84811c66f1ed6929901fe59fbfcf3eaaf
David Devore <drdcustomlows@gmail.com>
2018-05-31 08:33:18 -0400

    fix style bundle

--- src/PersonalSite/PersonalSite/App_Start/BundleConfig.cs
+++ src/PersonalSite/PersonalSite/App_Start/BundleConfig.cs
@@ -13,7 +13,7 @@ namespace PersonalSite.App_Start
             bundles.Add(new ScriptBundle("~/Scripts/jquery", "https://code.jquery.com/jquery-1.12.4.min.js").Include("~/Scripts/jquery-1.10.2.min.js"));
             bundles.Add(new ScriptBundle("~/Scripts/bootstrapjs", "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js").Include("~/Scripts/bootstrap.min.js"));
             bundles.Add(new ScriptBundle("~/Scripts/sitescripts").Include("~/Scripts/sitescripts.js"));
-            bundles.Add(new ScriptBundle("~/Content/bootstrapcss", "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css").Include("~/Content/bootstrap.min.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss", "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css").Include("~/Content/bootstrap.min.css"));
             bundles.Add(new StyleBundle("~/Content/sitecss").Include("~/Content/Site.css"));
 #if NDEBUG
             BundleTable.EnableOptimizations = true;
