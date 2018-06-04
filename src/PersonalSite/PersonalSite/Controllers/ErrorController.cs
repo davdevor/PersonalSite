0b6257c8db3df6ade73261bb9d3acb261255c608
David Devore <drdcustomlows@gmail.com>
2018-06-04 10:55:09 -0400

    add iis errors

--- src/PersonalSite/PersonalSite/Controllers/ErrorController.cs
+++ src/PersonalSite/PersonalSite/Controllers/ErrorController.cs
@@ -11,12 +11,14 @@ namespace PersonalSite.Controllers
         // GET: Error
         public ActionResult _404Error()
         {
+            ViewBag.title = "404 Error";
             Response.StatusCode = 404;
             return View();
         }
 
         public ActionResult _500Error()
         {
+            ViewBag.title = "500 Error";
             Response.StatusCode = 500;
             return View();
         }
