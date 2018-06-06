d9812a465a8211254f3a6af6dd16c9781b8b7f09
David Devore <drdcustomlows@gmail.com>
2018-06-06 10:49:15 -0400

    fix button going off screen and remove blank line from bottom of screen

--- src/PersonalSite/PersonalSite/Scripts/sitescripts/errorscripts.js
+++ src/PersonalSite/PersonalSite/Scripts/sitescripts/errorscripts.js
@@ -5,8 +5,8 @@
             // make the back button move when moused over
             $("#backbutton").click(goBack).mouseenter(function () {
                 $(this).animate({
-                    top: Math.random() * screen.height + "px",
-                    left: Math.random() * screen.width + "px",
+                    top: Math.random() * (window.innerHeight - $(this).height()) + "px",
+                    left: Math.random() * (window.innerWidth - $(this).width()) + "px",
                 });
             }).mouseenter(function () {
                 $(this).css("position", "absolute");
